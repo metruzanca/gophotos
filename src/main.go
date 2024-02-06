9340ea76a7df55490627d89b56c8090fa26dc64c
Sam Zanca <samuele@zanca.dev>
2024-02-06 14:53:58 -0500

    templ context example

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"github.com/labstack/echo/v4"
 	"github.com/metruzanca/go-photos/src/routes"
 )
@@ -9,9 +11,19 @@ const PORT = "3000"
 
 func main() {
 	app := echo.New()
+	app.Use(authMiddleware)
 
 	userRoute := routes.UserRoute{}
 	app.GET("/user", userRoute.UserList)
 
 	app.Start(":" + PORT)
+}
+
+func authMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// TODO replace with authentication
+		ctx := context.WithValue(c.Request().Context(), "user", "s@m.dev")
+		c.SetRequest(c.Request().WithContext(ctx))
+		return next(c)
+	}
 }
--- src/routes/user.go
+++ src/routes/user.go
@@ -2,11 +2,15 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/metruzanca/go-photos/src/model"
 	"github.com/metruzanca/go-photos/src/ui/user"
 )
 
 type UserRoute struct{}
 
 func (r UserRoute) UserList(c echo.Context) error {
-	return render(c, user.List())
+	u := model.User{
+		Email: "s@m.com",
+	}
+	return render(c, user.List(u))
 }
